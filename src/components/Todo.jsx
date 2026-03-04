ba51e3f886c19a817ea8a1b0c0a50cb22b62f56d
Anastasiya Rodnykh <rodnyhnastya@gmail.com>
2026-03-04 17:26:41 +0300

    Customize to-do tusks

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -5,13 +5,21 @@ import TodoList from "./todoList"
 
 
 const Todo = () => {
+    const tasks = [
+        {id: 'task-1', title: 'Task 1', isDone: false},
+        {id: 'task-2', title: 'Task 2', isDone: true},
+    ]
+
     return (
     <div className="todo">
         <h1 className="todo__title">To-Do List</h1>
         <AddTaskForm/>
         <SearchTaskForm/>
-        <TodoInfo/>
-        <TodoList/>
+        <TodoInfo
+            total={tasks.length}
+            done={tasks.filter(({isDone}) => isDone).length}
+        />
+        <TodoList tasks={tasks}/>
     </div>
     )
 }
