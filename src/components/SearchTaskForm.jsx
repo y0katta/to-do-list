25f8127914440d5abd4f0b7c7c6e497af48f4af0
Anastasiya Rodnykh <rodnyhnastya@gmail.com>
2026-03-05 17:38:30 +0300

    Search refactoring

--- src/components/SearchTaskForm.jsx
+++ src/components/SearchTaskForm.jsx
@@ -1,13 +1,23 @@
 import Field from "./Field"
 
-const SearchTaskForm = () => {
+const SearchTaskForm = (props) => {
+    const {
+        onSearchInput
+    } = props
+
     return (
+        <form 
+        className="todo__form"
+        onSubmit={(event) => event.preventDefault()} //отключение случайной отправки формы
+        >
         <Field
             className="todo__field"
             label="Search task"
             id="search-task"
             type="search"
+            onInput={(event) => onSearchInput(event.target.value)}
         />
+        </form>
     )
 }
 
