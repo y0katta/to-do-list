112f838fe515c8b4033bc86634274c12ccbcab30
Anastasiya Rodnykh <rodnyhnastya@gmail.com>
2026-03-05 17:39:00 +0300

    Add form refactoring

--- src/components/AddTaskForm.jsx
+++ src/components/AddTaskForm.jsx
@@ -1,13 +1,26 @@
 import Button from "./Button"
 import Field from "./Field"
 
-const AddTaskForm = () => { //рендеринг формы, используя Field и Button 
+const AddTaskForm = (props) => { //рендеринг формы, используя Field и Button 
+    const{
+        addTask,
+        newTaskTitle,
+        setNewTaskTitle,
+    }=props
+    
+    const onSubmit = (event) => {
+        event.preventDefault()
+        addTask()
+    }
+
     return(
-        <form className="todo__form">
+        <form className="todo__form" onSubmit={onSubmit}>
             <Field
                 className="todo__field"
                 label="New task title"
                 id="new-task"
+                value={newTaskTitle}
+                onInput={(event) => setNewTaskTitle(event.target.value)}
             />
             <Button type="submit">Add</Button>
         </form>
