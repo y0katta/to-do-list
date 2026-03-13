4af4b8a86ace5c80bdffd4a5f0d44878edd9a428
Anastasiya Rodnykh <rodnyhnastya@gmail.com>
2026-03-13 18:42:30 +0300

    Add message about none-existent tasks

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,31 +2,42 @@ import TodoItem from "./TodoItem"
 
 const TodoList = (props) => {
     const {
-      tasks = [], //массив объектов с полями title, id, isDone
-      onDeleteTaskButtonClick,
-      onTaskCompleteChange,
+        tasks = [], 
+        onDeleteTaskButtonClick,
+        onTaskCompleteChange,
+        filteredTasks,
+        firstIncompleteTaskRef,
+        firstIncompleteTaskId,
     } = props 
 
-    const hasTasks = true
+    const hasTasks = tasks.length > 0
+    const isEmptyFilteredTasks = filteredTasks?.length === 0
 
     if(!hasTasks) {
         return (
-            <div className="todo__empty-message"></div>
+            <div className="todo__empty-message">There are no tasks yet</div>
+        )
+    }
+
+    if(hasTasks && isEmptyFilteredTasks){
+        return (
+            <div className="todo__empty-message">Task not exists</div>
         )
     }
 
     return (
         <ul className="todo__list">
-            {tasks.map((task) => ( //Деструктурированная сущность task
-              <TodoItem
-              className="todo__item"
-              key={task.id}
-              {...task}
-              onDeleteTaskButtonClick={onDeleteTaskButtonClick}
-              onTaskCompleteChange={onTaskCompleteChange}
-              />
-              ))}
-      </ul>
+            {(filteredTasks ?? tasks).map((task) => ( 
+                <TodoItem
+                className="todo__item"
+                key={task.id}
+                {...task}
+                ref={task.id === firstIncompleteTaskId ? firstIncompleteTaskRef : null}
+                onDeleteTaskButtonClick={onDeleteTaskButtonClick}
+                onTaskCompleteChange={onTaskCompleteChange}
+                />
+            ))}
+        </ul>
     )
 }
 
