d5b6cc9664640c25c3bbd527f9bec5bb973ed4f7
Anastasiya Rodnykh <rodnyhnastya@gmail.com>
2026-03-19 18:35:55 +0300

    Refactoring with routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import Todo from "./components/Todo"
-import { TasksProvider } from "./context/TasksContext"
+import Router from "./Router"
+import TasksPage from "./pages/TasksPage"
+import TaskPage from "./pages/TaskPage"
 
 const App = () => {
+  const routes = {
+      '/': TasksPage,
+      '/tasks/:id': TaskPage,
+      '*': () => <div>404 page not found</div>
+  }
+
   return (
-    <TasksProvider>
-      <Todo/>
-    </TasksProvider>
+    <Router 
+    routes={routes}
+    />
   )
 }
 
